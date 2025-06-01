507d932db6438fd68d17a458d54f2a8b1aecc8ab
Alvis Abreo <68607388+cratonoid@users.noreply.github.com>
2025-06-01 16:00:43 +0530

    new code

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const customerRoutes = require("./routes/customerRoutes");
-const path = require('path');
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // to parse JSON
+// app.use(express.json()); // to parse JSON
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the public folder (assuming public is sibling to src)
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongoDB connected")
+    console.log("MongoDB connected");
     require("./scheduler/addressReminderScheduler");
   })
   .catch((err) => console.error("MongoDB connection error:", err));
